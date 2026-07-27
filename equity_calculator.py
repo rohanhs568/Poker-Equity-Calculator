ba80b8e41ffc06bafab97b361f29126a68f81608
Rohan Sanghrajka <209335657+rohanhs568@users.noreply.github.com>
2026-07-27 19:25:08 +0100

    initial commit: hand strength calculator and equity simulator

---
+++ equity_calculator.py
@@ -0,0 +1,46 @@
+from strength_calculator import base14, hand_strength_calculator
+
+rank = [0,1,2,3,4,5,6,7,8,9,10,11,12] 
+suit = [0, 1, 2, 3]
+
+from itertools import product
+deck = list(product(rank,suit))
+
+import random
+
+def equity_calculator(hand1, hand2, no_of_trials):
+    hand1_wins = 0
+    hand2_wins = 0
+
+    deck = list(product(rank, suit))
+
+    for card in hand1:
+        deck.remove(card)
+    for card in hand2:
+        deck.remove(card)
+
+    for _ in range(no_of_trials):
+        board = random.sample(deck, 5)
+
+        hand1_strength = hand_strength_calculator(hand1, board)
+        hand2_strength = hand_strength_calculator(hand2, board)
+
+        if hand1_strength > hand2_strength:
+            hand1_wins += 1
+
+        elif hand1_strength < hand2_strength:
+            hand2_wins += 1
+
+        else:
+            hand1_wins += 0.5
+            hand2_wins += 0.5
+
+    return hand1_wins, hand2_wins
+
+print(equity_calculator([(12,0), (12,1)], [(12,2),(12,3)], 1000000))
+         
+
+
+        
+
+
---
+++ strength_calculator.py
@@ -0,0 +1,130 @@
+def base14(values):
+        if len(values) > 5:
+            raise ValueError("too many values")
+
+        pad_values = values + [0] * (5 - len(values))
+
+        base14_output = 0
+        for i in range(5):
+            base14_output += pad_values[i] * 14**(4 - i)    
+
+        return base14_output
+
+def hand_strength_calculator(hand, community_cards):
+
+    strength = 0
+
+    test_board = hand + community_cards
+
+    suit_counter = [0,0,0,0]
+    for card in test_board:
+        suit_counter[card[1]] += 1
+
+    rank_counter = [0 for i in range(13)]
+    for card in test_board:
+        rank_counter[card[0]] += 1
+
+    flush_exists = False
+    found_straight_flush = False
+    # straight flush
+    for suit_index, suit_count in enumerate(suit_counter):
+        if suit_count >= 5:
+            flush_exists = True
+            flush = [card for card in test_board if card[1] == suit_index]
+
+            flush_rank_counter = [0 for i in range(13)]
+            for card in flush:
+                flush_rank_counter[card[0]] += 1
+
+            for high in range(12,3,-1):
+                if flush_rank_counter[high-4:high+1] == [1,1,1,1,1]:
+                    strength = base14([9, high])
+                    found_straight_flush = True
+                    
+                    break
+
+            if found_straight_flush == False and flush_rank_counter[12] == flush_rank_counter[0] == flush_rank_counter[1] == flush_rank_counter[2] == flush_rank_counter[3] == 1:
+                strength = base14([9, 5])
+                found_straight_flush = True
+
+        if flush_exists == True:
+            break
+
+    # record all pairing data
+
+    quads = None
+    trips_list = []
+    pairs_list = []
+
+    for i in range(12,-1,-1):
+        r = rank_counter[i]
+
+        if r == 4:
+            quads = i
+
+        if r == 3:
+            trips_list.append(i)
+
+        if r == 2:
+            pairs_list.append(i)
+
+
+    highest_trips = trips_list[0] if len(trips_list)>0 else None
+    highest_pair = pairs_list[0] if len(pairs_list)>0 else None
+    second_highest_pair = pairs_list[1] if len(pairs_list) > 1 else None
+
+    # full house
+    fullhouse_pair_component = None
+    if highest_trips is not None:
+        other_candidates = [t for t in trips_list if t != highest_trips] + pairs_list
+        if len(other_candidates)>0:
+            fullhouse_pair_component = max(other_candidates)
+
+    # straight 
+    straight_found = False
+    straight_high = None
+    if not found_straight_flush:
+        for high in range(12,3,-1):
+            for j in range(4,-1,-1):
+                if rank_counter[high - j] == 0:
+                    break
+            else:
+                straight_found = True
+                straight_high = high
+                break
+
+    # wheel 
+    wheel_found = False
+    if not found_straight_flush and straight_found == False and rank_counter[0] > 0 and rank_counter[1] > 0 and rank_counter[2] > 0 and rank_counter[3] > 0 and rank_counter[12] > 0:
+        wheel_found = True
+
+    if not found_straight_flush:
+        if quads is not None:
+            strength = base14([8, quads])
+
+        elif fullhouse_pair_component is not None:
+            strength = base14([7, highest_trips, fullhouse_pair_component])
+
+        elif flush_exists == True:
+            flush_ranks = sorted([card[0] for card in flush], reverse=True)[:4]
+            strength = base14([6] + flush_ranks)
+
+        elif straight_found == True:
+            strength = base14([5, straight_high])
+
+        elif wheel_found == True:
+            strength = base14([5, 5])
+
+        elif highest_trips is not None:
+            strength = base14([4, highest_trips])
+
+        elif highest_pair is not None and second_highest_pair is not None:
+            strength = base14([3, highest_pair, second_highest_pair])
+
+        elif highest_pair is not None:
+            strength = base14([2, highest_pair])
+
+        else:
+            strength = base14([1])
+
+    return strength
